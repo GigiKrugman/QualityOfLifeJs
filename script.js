a8a57967982a4c81e09962423ac97c9683f6d535
Marcello Mulas <marcellomulas93@gmail.com>
2023-03-06 16:00:22 +0100

    Added Style and changed some JS

--- script.js
+++ script.js
@@ -12,17 +12,20 @@ function getCityData(city) {
   )
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       displayData(data);
     });
-  // fetch(`https://api.teleport.org/api/urban_areas/?slug:${city}/images/`)
+  // fetch(`(https://api.teleport.org/api/urban_areas/?embed=ua:item/ua:images`)
   //   .then((response) => response.json())
   //   .then((image) => {
   //     console.log(image);
   //   });
 }
 
-function displayData(data) {
+function displayData(data, image) {
   const cityData = data._embedded["city:search-results"];
+  const cityImage = image;
+  console.log(cityImage);
 
   //console.log(cityData);
   const urbanArea =
@@ -37,17 +40,21 @@ function displayData(data) {
   const qualityOfLife = urbanArea.teleport_city_score;
 
   let results = document.getElementById("results-list-categories");
-  results.innerHTML += "<h2>Categories:</h2><ul>";
+  results.innerHTML += `<h2 class="header--js" >Categories:</h2><ul>`;
   categories.forEach((category) => {
-    results.innerHTML += `<ul><li>${category.name}: ${category.score_out_of_10}</li></ul>`;
+    results.innerHTML += `<div class="categories--div"><ul><li class="categories--list--js">${
+      category.name
+    }: ${category.score_out_of_10.toFixed(2)} / 10</li></ul></div>`;
   });
   //results += "</ul>";
   document.getElementById(
     "results-list-summary"
-  ).innerHTML += `<h3>Summary:</h3> ${descriptions}`;
+  ).innerHTML += `<div class="description--js"><h3>Summary:</h3><p> ${descriptions} </p></div>`;
   document.getElementById(
     "results-list-score"
-  ).innerHTML += `<h4>Quality of Life:</h4> ${qualityOfLife}`;
+  ).innerHTML += `<div class="quality--percentage--js"><h4>Quality of Life:</h4> ${qualityOfLife.toFixed(
+    1
+  )} points out of 100</div>`;
 
   //document.getElementById("results-list").innerHTML = results;
 }
